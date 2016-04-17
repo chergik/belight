12e5e74b104a9f90b080e916a8cf8a2b9cbe79bb
Andrey Chergik <achergik@gmail.com>
2016-04-16 19:36:28 -0700

    Launcher removed. It may be used later if we waht to talk to Alexa from mobile phone

--- src/main/java/belight/HelloWorldSpeachletRequestStreamHandler.java
+++ src/main/java/belight/HelloWorldSpeachletRequestStreamHandler.java
@@ -14,7 +14,7 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 /**
  * This class could be the handler for an AWS Lambda function powering an Alexa Skills Kit
  * experience. To do this, simply set the handler field in the AWS Lambda console to
- * "HelloWorldSpeechletRequestStreamHandler" For this to work, you'll also need to build
+ * "HelloWorldSpeachletRequestStreamHandler" For this to work, you'll also need to build
  * this project using the {@code lambda-compile} Ant task and upload the resulting zip file to power
  * your function.
  */
